7425b546eb30fe5d085d687c5be92b952fb22e7b
Muniyasamy Baskaran <muniyasamy.baskaran@ideas2it.com>
2025-05-13 14:52:19 +0530

    patient-modile

--- src/main/java/com/patient/module/domain/dto/PatientRequest.java
+++ src/main/java/com/patient/module/domain/dto/PatientRequest.java
@@ -2,17 +2,22 @@ package com.patient.module.domain.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class PatientRequest {
 
     private String medicalRecordNumber;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate startOfCareDate;
     private String status;
     private String firstName;
     private String lastName;
     private String sex;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate birthDate;
     private String maritalStatus;
     private String address;
